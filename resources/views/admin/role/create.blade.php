40f670741130bd35414d4292ce2d51507d5e0d36
dong-nguyen-1994 <67553313+dong-nguyen-1994@users.noreply.github.com>
2021-03-30 11:29:54 +0700

    Update create.blade.php

--- resources/views/admin/role/create.blade.php
+++ resources/views/admin/role/create.blade.php
@@ -18,51 +18,51 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <form role="form" action="{{ route('admin.role.store') }}" method="POST">
-            @csrf
-            <div class="row">
-                <!-- left column -->
-                <div class="col-md-6">
-                    <!-- general form elements -->
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h3 class="card-title">{{ __('acl::role.create.page_title') }}</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <!-- form start -->
-
-                        @include('acl::admin.role._field')
-
-                        <!-- /.card-body -->
-
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">{{ __('core::button.save') }}</button>
-                            <button class="btn btn-secondary" name="continue" value="1" type="submit">{{ __('core::button.save_and_edit') }}</button>
-                        </div>
+    
+    <form role="form" action="{{ route('admin.role.store') }}" method="POST">
+        @csrf
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-6">
+                <!-- general form elements -->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ __('acl::role.create.page_title') }}</h3>
                     </div>
-                    <!-- /.card -->
-                </div>
-                <div class="col-md-6">
-{{--                    <dnsoft-tree></dnsoft-tree>--}}
-                    <!-- general form elements -->
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h3 class="card-title">{{ __('acl::role.permissions') }}</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <!-- form start -->
-                        <div class="card-body">
-                            <dnsoft-tree
-                                name="permissions"
-                                :data='@json(\Dnsoft\Acl\Facades\Permission::allTreeWithoutKey())'
-                                :value='@json(json_decode(object_get($item, 'permissions')))'
-                            ></dnsoft-tree>
-                        </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+
+                    @include('acl::admin.role._field')
+
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">{{ __('core::button.save') }}</button>
+                        <button class="btn btn-secondary" name="continue" value="1" type="submit">{{ __('core::button.save_and_edit') }}</button>
                     </div>
-                    <!-- /.card -->
                 </div>
+                <!-- /.card -->
             </div>
-        </form>
-    </div>
+            <div class="col-md-6">
+{{--                    <dnsoft-tree></dnsoft-tree>--}}
+                <!-- general form elements -->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ __('acl::role.permissions') }}</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <div class="card-body">
+                        <dnsoft-tree
+                            name="permissions"
+                            :data='@json(\Dnsoft\Acl\Facades\Permission::allTreeWithoutKey())'
+                            :value='@json(json_decode(object_get($item, 'permissions')))'
+                        ></dnsoft-tree>
+                    </div>
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+    </form>
+    
 @stop
