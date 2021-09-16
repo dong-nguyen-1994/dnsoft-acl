e4aba70161c6ff86012a515c6a8c5b8a3d38755e
Dong Nguyen <dongbuh@gmail.com>
2021-09-16 16:27:42 +0700

    Update text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <title>Dnsoft - Admin Dashboard</title>
+    <title>Dnsoft - Administration System Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
     <meta content="Coderthemes" name="author">
@@ -19,86 +20,79 @@
 
 <body>
 
-<div class="account-pages mt-5 mb-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-6 col-xl-5">
-                <div class="card">
+    <div class="account-pages mt-5 mb-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card">
 
-                    <div class="card-body p-4">
+                        <div class="card-body p-4">
 
-                        <div class="text-center w-75 m-auto">
-                            <a href="#">
-                                <span><img src="{{ asset('vendor/dnsoft/admin/img/logo.png') }}" alt="" width="180"></span>
-                            </a>
-                            <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
-                        </div>
+                            <div class="text-center w-75 m-auto">
+                                <a href="#">
+                                    <span><img src="{{ asset('vendor/dnsoft/admin/img/logo.png') }}" alt="" width="180"></span>
+                                </a>
+                                <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
+                            </div>
 
-                        <form action="{{ route('admin.login') }}" method="post">
-                            @csrf
-                            <div class="form-group mb-3">
-                                <label for="emailaddress">Email address</label>
-                                <input  class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" type="email" name="email" required="" placeholder="Enter your email">
-                                @error('email')
+                            <form action="{{ route('admin.login') }}" method="post">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <label for="emailaddress">Email address</label>
+                                    <input class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" type="email" name="email" required="" placeholder="Enter your email">
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                                    @enderror
+                                </div>
 
-                            <div class="form-group mb-3">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter your password">
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
+                                <div class="form-group mb-3">
+                                    <label for="password">Password</label>
+                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter your password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked="">
-                                    <label class="custom-control-label" for="checkbox-signin">Remember me</label>
+                                    @enderror
                                 </div>
-                            </div>
 
-                            <div class="form-group mb-0 text-center">
-                                <button class="btn btn-primary btn-block" type="submit"> Log In </button>
-                            </div>
+                                <div class="form-group mb-3">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked="">
+                                        <label class="custom-control-label" for="checkbox-signin">Remember me</label>
+                                    </div>
+                                </div>
 
-                        </form>
+                                <div class="form-group mb-0 text-center">
+                                    <button class="btn btn-primary btn-block" type="submit"> Log In </button>
+                                </div>
 
-                    </div> <!-- end card-body -->
-                </div>
-                <!-- end card -->
+                            </form>
 
-                <div class="row mt-3">
-                    <div class="col-12 text-center">
-                        <p> <a href="pages-recoverpw.html" class="text-muted ml-1">Forgot your password?</a></p>
-                        <p class="text-muted">Don't have an account? <a href="pages-register.html" class="text-primary font-weight-medium ml-1">Sign Up</a></p>
-                    </div> <!-- end col -->
-                </div>
-                <!-- end row -->
+                        </div> <!-- end card-body -->
+                    </div>
+                    <!-- end card -->
 
-            </div> <!-- end col -->
+                </div> <!-- end col -->
+            </div>
+            <!-- end row -->
         </div>
-        <!-- end row -->
+        <!-- end container -->
     </div>
-    <!-- end container -->
-</div>
-<!-- end page -->
+    <!-- end page -->
 
 
-<footer class="footer footer-alt">
-    2016 - 2019 &copy; Minton theme by <a href="" class="text-muted">Coderthemes</a>
-</footer>
+    <footer class="footer footer-alt">
+        2016 - 2019 &copy; Minton theme by <a href="" class="text-muted">Coderthemes</a>
+    </footer>
 
-<!-- Vendor js -->
-<script src="{{ asset('vendor\\dnsoft\\admin\\') }}assets\js\vendor.min.js"></script>
+    <!-- Vendor js -->
+    <script src="{{ asset('vendor\\dnsoft\\admin\\') }}assets\js\vendor.min.js"></script>
 
-<!-- App js -->
-<script src="{{ asset('vendor\\dnsoft\\admin\\') }}assets\js\app.min.js"></script>
+    <!-- App js -->
+    <script src="{{ asset('vendor\\dnsoft\\admin\\') }}assets\js\app.min.js"></script>
 
 </body>
+
 </html>
