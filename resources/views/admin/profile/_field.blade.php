006fbea31db37cbd5f6387f22e95ab9d38b0599b
Dong Nguyen <67553313+nguyen-van-dong@users.noreply.github.com>
2022-11-24 01:01:58 +0700

    Update _field.blade.php

--- resources/views/admin/profile/_field.blade.php
+++ resources/views/admin/profile/_field.blade.php
@@ -9,10 +9,9 @@
             @input(['name' => 'password_confirmation', 'type' => 'password', 'label' => __('acl::profile.password_confirmation')])
         </div>
     </div>
-{{--    @sumoselect(['name' => 'role', 'label' => 'Roles', ])--}}
     @sumoselect(['name' => 'roles', 'label' => __('acl::role.page_title'), 'multiple' => true, 'options' => get_roles_options()])
 
     @checkbox(['name' => 'is_admin', 'label' => '' , 'placeholder' => __('acl::profile.is_admin')])
 
-    @mediafile(['name' => 'avatar', 'label' => __('acl::profile.avatar')])
+    @singleFile(['name' => 'avatar', 'type' => 'file', 'label' => __('acl::profile.avatar'), 'idHolder' => 'avatarHolder', 'files' => 'avatar'])
 </div>
