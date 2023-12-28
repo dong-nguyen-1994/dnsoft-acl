9a3a78f01364b84c729b3a8af265cda3c32cf521
nguyen-van-dong <dongbuh@gmail.com>
2023-12-28 15:27:28 +0700

    Remove version

--- resources/views/v1/admin/role/_field.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="card-body">
-    @input(['name' => 'name', 'label' => __('acl::role.name'), 'require' => true])
-    @input(['name' => 'display_name', 'label' => __('acl::role.display_name'), 'require' => true])
-    @textarea(['name' => 'description', 'label' => __('acl::role.description')])
-</div>
